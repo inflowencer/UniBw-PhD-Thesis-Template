7ad57494d7d4b3f42a3ec41a3cbe242983c16a77
inflowencer <maximilian.maigler@unibw.de>
2024-04-18 11:12:01 +0200

    Fixed los and loa escape characters

--- src/sorts.py
+++ src/sorts.py
@@ -10,9 +10,12 @@ for i, line in enumerate(lines):
         df_los = df_los.sort_values(by="description")
         los_str = "los:\n"
         for index, row in df_los.iterrows():
-            los_str += r"""- sym: %s
-  abb: %s
-  descr: %s
+            los_str += r"""- sym: |-
+    %s
+  unit: |-
+    %s
+  descr: |-
+    %s
 """ % (row["symbol"], row["unit"], row["description"])
 
         lines[i] = los_str
@@ -25,9 +28,12 @@ for i, line in enumerate(lines):
         df_loa = df_loa.sort_values(by="abbreviation")
         loa_str = "loa:\n"
         for index, row in df_loa.iterrows():
-            loa_str += r"""- abb: %s
-  short: %s
-  long: %s
+            loa_str += r"""- abb: |-
+    %s
+  short: |-
+    %s
+  long: |-
+    %s
 """ % (row["abbreviation"], row["short"], row["long"])
 
         lines[i] = "\n" + loa_str
